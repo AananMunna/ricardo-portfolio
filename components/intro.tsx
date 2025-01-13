66a0c8f3e1af798b4ba7f18d676bf9e9e03f2360
unknown <aananmunna420@gmail.com>
2025-01-13 15:43:03 +0600

    second commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,10 +62,10 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="font-bold">Hello, I'm Aanan Munna.</span> I'm a{" "}
+        <span className="font-bold">Front-End developer</span> with{" "}
+        <span className="font-bold">4 years</span> of experience. I enjoy
+        building <span className="italic">responsive sites</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
